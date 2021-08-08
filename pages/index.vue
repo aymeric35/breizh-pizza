896154cc7f18af67585933d3ae380da295f24ab2
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-08 16:41:18 +0200

    Setup project

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="body">
+    <TheHeader></TheHeader>
+    <vue100vh></vue100vh>
+    <TheFooter></TheFooter>
+  </div>
+</template>
+
+<script>
+import vue100vh from 'vue-100vh'
+
+export default {
+  components: { vue100vh },
+}
+</script>
---
+++ windi.config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'windicss/helpers'
+
+export default defineConfig({
+    darkMode: false,
+    theme: {
+        container: {
+            center: true,
+            padding: {
+                DEFAULT: "1rem",
+            },
+        },
+        extend: {},
+    },
+    plugins: [],
+})
