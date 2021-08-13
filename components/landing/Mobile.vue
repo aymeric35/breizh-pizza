c4d212e2dbe7115a336d4e5ff191f91614e459fe
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-13 23:09:37 +0200

    Create Mobile.vue

---
+++ components/landing/Mobile.vue
@@ -0,0 +1,84 @@
+<template>
+  <section class="h-full -mt-12 bg-cover bg-body-pattern md:hidden">
+    <div class="container flex flex-col h-full pt-12">
+      <h1
+        class="mt-auto text-4xl text-center text-gray-50 font-heading sm:text-6xl"
+      >
+        Des pizzas <span class="text-orange-500">artisanales</span>
+      </h1>
+      <figure class="mt-auto">
+        <picture>
+          <source
+            :data-srcset="webp.pizza.lg"
+            media="(min-width:640px)"
+            type="image/webp"
+          />
+          <source
+            :data-srcset="png.pizza.lg"
+            media="(min-width:640px)"
+            type="image/png"
+          />
+          <source :data-srcset="webp.pizza.sm" type="image/webp" />
+          <img
+            :data-src="png.pizza.sm"
+            class="w-full mx-auto lazyload x-sm:w-8/10 sm:w-1/2 image-rendering-opt"
+          />
+        </picture>
+      </figure>
+      <p class="mt-auto text-lg text-center text-gray-50 font-body">
+        Découvrez nos pizzas savoureuses cuites au feu de bois et réalisées à
+        partir d’ingrédients locaux.
+      </p>
+      <div
+        class="w-screen mt-auto relative left-1/2 -ml-[-50vw] flex flex-col"
+      >
+        <button
+          type="button"
+          class="py-2 text-lg bg-orange-500 text-gray-50 font-body focus:outline-none"
+        >
+          <NuxtLink to="/contact">Commander</NuxtLink>
+        </button>
+        <button
+          type="button"
+          class="py-2 text-lg bg-green-500 text-gray-50 font-body focus:outline-none"
+        >
+          <NuxtLink to="/contact">Réserver</NuxtLink>
+        </button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import PizzaSmWebp from '~/assets/images/homepage/pizza-m.webp'
+import PizzaLgWebp from '~/assets/images/homepage/pizza-hd.webp'
+import PizzaSmPng from '~/assets/images/homepage/pizza-m.png'
+import PizzaLgPng from '~/assets/images/homepage/pizza-hd.png'
+
+export default {
+  data() {
+    return {
+      webp: {
+        pizza: {
+          sm: PizzaSmWebp,
+          lg: PizzaLgWebp,
+        },
+      },
+      png: {
+        pizza: {
+          sm: PizzaSmPng,
+          lg: PizzaLgPng,
+        },
+      },
+    }
+  },
+}
+</script>
+
+<style scoped>
+@layer utilities {
+  .image-rendering-opt {
+    image-rendering: -webkit-optimize-contrast;
+  }
+}
+</style>
