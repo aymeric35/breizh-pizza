f6b577ab1f2fe49c21a8d374ca77c183e3581b36
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-13 23:09:40 +0200

    Create TheCTA.vue

---
+++ components/TheCTA.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="bg-gray-900 text-gray-50 text-center">
+        <div class="container bg-cta-order-m h-[50vh] bg-cover bg-blend-overlay flex flex-col justify-center items-center space-y-4">
+            <h2 class="font-heading text-4xl">Commandez votre pizza</h2>
+            <p class="font-heading text-2xl">En livraison ou à emporter</p>
+            <button class="rounded-xl bg-orange-500 py-0.5 font-body text-xl px-4">Commander</button>
+        </div>
+        <div class="container bg-cta-book-m h-[50vh] bg-cover bg-blend-overlay flex flex-col justify-center items-center space-y-4">
+            <h2 class="font-heading text-4xl">Réservez une table</h2>
+            <p class="font-heading text-2xl">et découvrez l’ambiance chaleureuse de notre restaurant </p>
+            <button class="rounded-xl bg-orange-500 py-0.5 font-body text-xl px-4">Réserver</button>
+        </div>
+    </section>
+</template>
