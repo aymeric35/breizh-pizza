922acdda6832206c523113a561583002ffcb670a
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-13 23:09:45 +0200

    Update TheFooter.vue

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,8 @@
 <template>
-    <footer>
-
+    <footer class="bg-gray-900 text-gray-400 font-body py-4">
+        <div class="container flex flex-col justify-center items-center space-y-2">
+            <p>Copyright © Aymeric</p>
+            <NuxtLink to="/">Mentions Légales</NuxtLink>
+        </div>
     </footer>
 </template>
