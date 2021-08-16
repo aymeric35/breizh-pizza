de8dd76e16ea90795ef3316ab4d227c91551cb38
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-16 23:04:06 +0200

    Create mentions-légales.vue

---
+++ pages/mentions-légales.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="bg-orange-900">
+    <TheHeader></TheHeader>
+    <vue100vh>
+      <div class="container flex flex-col h-screen items-center justify-center">
+        <h1 class="font-heading text-center text-4xl">Attributions</h1>
+        <a href="https://thenounproject.com/term/divider/1070821/" class="h-1/2"
+          ><img class="h-full" :src="divider" alt="Divider"
+        /></a>
+      </div>
+    </vue100vh>
+  </div>
+</template>
+
+<script>
+import vue100vh from 'vue-100vh'
+import Divider from '~/assets/images/menu/divider.svg'
+export default {
+  components: { vue100vh },
+  data() {
+    return {
+      divider: Divider,
+    }
+  },
+}
+</script>
