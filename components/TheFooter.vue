647b6204801b1758e762302376c88e72760b6efe
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-16 23:03:46 +0200

    Add path to NuxtLink

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -2,7 +2,7 @@
     <footer class="bg-dark-900 text-gray-400 font-body py-4">
         <div class="container flex flex-col justify-center items-center space-y-2 sm:flex-row sm:space-y-0 sm:justify-between">
             <p>Copyright © Aymeric</p>
-            <NuxtLink to="/">Mentions Légales</NuxtLink>
+            <NuxtLink to="/mentions-légales">Mentions Légales</NuxtLink>
         </div>
     </footer>
 </template>
