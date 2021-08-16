a89536330ba1310e8028332b552bdb0ad3cee43e
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-16 16:52:45 +0200

    Add responsive

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,6 +1,6 @@
 <template>
-    <footer class="bg-gray-900 text-gray-400 font-body py-4">
-        <div class="container flex flex-col justify-center items-center space-y-2">
+    <footer class="bg-dark-900 text-gray-400 font-body py-4">
+        <div class="container flex flex-col justify-center items-center space-y-2 sm:flex-row sm:space-y-0 sm:justify-between">
             <p>Copyright © Aymeric</p>
             <NuxtLink to="/">Mentions Légales</NuxtLink>
         </div>
