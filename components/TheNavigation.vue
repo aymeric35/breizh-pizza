ed119dc95d97ea23f80905fab2cce3ba68853f8d
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-16 16:53:07 +0200

    Update TheNavigation.vue

--- components/TheNavigation.vue
+++ components/TheNavigation.vue
@@ -1,13 +1,13 @@
 <template>
   <div
-    class="fixed top-0 left-0 z-40 flex flex-col items-center justify-center w-screen h-screen overflow-hidden transition-opacity bg-gray-900 text-gray-50 animate-slide-in-right animate-duration-500 md:flex md:items-center md:static md:bg-transparent md:animate-none md:h-auto md:w-auto"
+    class="fixed top-0 left-0 z-40 flex flex-col items-center justify-center w-screen h-screen overflow-hidden transition-opacity bg-gray-900 text-gray-50 animate-slide-in-right animate-duration-500 lg:flex lg:items-center lg:static lg:bg-transparent lg:animate-none lg:h-auto lg:w-auto"
   >
     <nav>
-      <ul class="text-4xl text-center space-y-15 font-heading md:space-y-0 md:flex md:flex-row md:justify-around md:items-center md:space-x-10 md:font-body md:text-xl lg:space-x-15 lg:text-2xl xl:space-x-20">
+      <ul class="text-4xl text-center space-y-15 font-heading lg:space-y-0 lg:flex lg:flex-row lg:justify-around lg:items-center lg:space-x-10 lg:font-body lg:text-xl lg:space-x-15 lg:text-2xl xl:space-x-20">
         <li><NuxtLink to="/" @click.native="clearScroll">Accueil</NuxtLink></li>
         <li><NuxtLink to="/menu" @click.native="clearScroll">Menu</NuxtLink></li>
         <li><NuxtLink to="/contact" @click.native="clearScroll">Contact</NuxtLink></li>
-        <li class="hidden md:px-5 md:py-0.5 md:block md:bg-orange-500 md:rounded-full"><NuxtLink to="/contact" @click.native="clearScroll">Commander</NuxtLink></li>
+        <li class="hidden lg:px-5 lg:py-0.5 lg:block lg:bg-orange-500 lg:rounded-full"><NuxtLink to="/contact" @click.native="clearScroll">Commander</NuxtLink></li>
       </ul>
     </nav>
   </div>
