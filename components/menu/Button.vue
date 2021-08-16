73d2830073e84bd47f0ce3ef2c059e932c62f74e
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-16 23:03:12 +0200

    Update Button.vue

--- components/menu/Button.vue
+++ components/menu/Button.vue
@@ -1,3 +1,18 @@
 <template>
-    <button class="bg-orange-900 text-orange-500"> {{ message }} </button>
+  <a :href="tag" class="bg-orange-900 text-orange-500 font-body rounded-full px-4 bg-opacity-40">{{ message }}</a>
 </template>
+
+<script>
+export default {
+  props: {
+    message: {
+      type: String,
+      required: true,
+    },
+    tag: {
+      type: String,
+      required: true,
+    }
+  },
+}
+</script>
