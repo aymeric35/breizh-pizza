0a888ce2a4dec262c169288cba745a3afb51de1c
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-19 23:21:33 +0200

    Remove lazyload, Switch buttons to components

--- components/landing/Desktop.vue
+++ components/landing/Desktop.vue
@@ -1,47 +1,41 @@
 <template>
-  <section class="hidden h-full -mt-16 bg-cover lg:block bg-body-pattern-2x xl:-mt-24">
-    <div class="container flex items-center justify-center h-full pt-12 space-x-30">
+  <section
+    class="hidden h-full -mt-16 bg-cover lg:block bg-body-pattern-2x xl:-mt-24"
+  >
+    <div
+      class="container flex items-center justify-center h-full pt-12 space-x-30"
+    >
       <div class="w-1/2 space-y-3">
-        <h1
-          class="text-4xl text-6xl text-gray-50 font-heading"
-        >
+        <h1 class="text-4xl text-6xl text-gray-50 font-heading">
           Des pizzas <span class="text-orange-500">artisanales</span>
         </h1>
         <p class="text-lg text-gray-50 font-body">
           Découvrez nos pizzas savoureuses cuites au feu de bois et réalisées à
           partir d’ingrédients locaux.
         </p>
-        <div
-          class="flex-inline"
-        >
-          <button
-            type="button"
-            class="px-6 py-2 text-lg bg-orange-500 rounded-full text-gray-50 font-body focus:outline-none"
-          >
-            <NuxtLink to="/contact">Commander</NuxtLink>
-          </button>
-          <button
-            type="button"
-            class="px-6 py-2 ml-4 text-lg text-green-500 rounded-full ring ring-green-500 text-gray-50 font-body focus:outline-none"
-          >
-            <NuxtLink to="/contact">Réserver</NuxtLink>
-          </button>
+        <div class="space-x-6">
+          <PrimaryButton label="Commander" destination="/contact"
+            ><ButtonIconCouverts></ButtonIconCouverts
+          ></PrimaryButton>
+          <SecondaryButton label="Réserver" destination="/contact"
+            ><ButtonIconChevron></ButtonIconChevron
+          ></SecondaryButton>
         </div>
       </div>
       <figure class="w-1/2">
         <picture>
           <source
-            :data-srcset="webp.pizza.xl"
+            :srcset="webp.pizza.xl"
             media="(min-width:2000px)"
             type="image/webp"
           />
           <source
-            :data-srcset="png.pizza.xl"
+            :srcset="png.pizza.xl"
             media="(min-width:2000px)"
             type="image/png"
           />
-          <source :data-srcset="webp.pizza.lg" type="image/webp" />
-          <img :data-src="png.pizza.lg" class="w-full lazyload image-rendering-opt" />
+          <source :srcset="webp.pizza.lg" type="image/webp" />
+          <img :src="png.pizza.lg" class="w-full image-rendering-opt" />
         </picture>
       </figure>
     </div>
