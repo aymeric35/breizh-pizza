470ba0bac41ce4923eeb7d2cf0f7b4c652d7b992
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-19 23:20:57 +0200

    Create SecondaryButton.vue

---
+++ components/SecondaryButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <NuxtLink
+    :to="destination"
+    class="rounded-lg flex font-body shadow-md ring text-lg py-2 px-6 transition-all ring-green-500 text-green-500 gap-2 duration-300 items-center inline-flex hover:ring-green-600 hover:fill-green-600 hover:text-green-600 focus:outline-none"
+  >
+    {{ label }}
+    <slot></slot>
+  </NuxtLink>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    destination: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
