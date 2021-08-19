697b3097f9e72acb4c4d8e4202af05ce7771a502
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-19 23:23:18 +0200

    Remove lazyload

--- pages/menu.vue
+++ pages/menu.vue
@@ -1,27 +1,27 @@
 <template>
   <div class="bg-gray-900">
     <TheHeader></TheHeader>
-    <section class="container text-gray-50">
+    <section class="container text-gray-50 relative">
       <h1 class="font-heading text-center py-8 text-3xl">Menu</h1>
       <figure>
         <picture>
           <source
-            :data-srcset="menu.ret"
+            :srcset="menu.ret"
             media="(min-width:2000px)"
             type="image/jpg"
           />
           <source
-            :data-srcset="menu.lg"
+            :srcset="menu.lg"
             media="(min-width:640px)"
             type="image/jpg"
           />
           <img
-            :data-src="menu.sm"
-            class="rounded-xl object-cover object-bottom h-30 shadow-md w-screen lazyload image-rendering-opt lg:h-[40vh] 2xl:h-[60vh]"
+            :src="menu.sm"
+            class="rounded-xl object-cover object-bottom h-30 shadow-md w-screen image-rendering-opt lg:h-[40vh] 2xl:h-[60vh]"
           />
         </picture>
       </figure>
-      <div class="text-center pt-3 pb-4 overflow-x-scroll whitespace-nowrap sm:flex sm:justify-between sm:overflow-hidden lg:hidden">
+      <div class="text-center sticky top-0 pt-3 pb-4 overflow-x-scroll whitespace-nowrap sm:flex sm:justify-between sm:overflow-hidden lg:hidden">
         <MenuButton tag="#entrées" message="Entrées"></MenuButton>
         <MenuButton tag="#pizzas" message="Pizzas"></MenuButton>
         <MenuButton tag="#desserts" message="Desserts"></MenuButton>
