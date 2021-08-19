f70f533ab56e76f32eccfc2db06c136d012b319d
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-19 23:19:40 +0200

    Create button icon components

---
+++ components/ButtonIcon/Chevron.vue
@@ -0,0 +1,13 @@
+<template>
+  <svg
+    width="7"
+    height="11"
+    viewBox="0 0 7 11"
+    fill="currentColor"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M1.83345 0.437057L0.658447 1.61206L4.47511 5.43706L0.658447 9.26206L1.83345 10.4371L6.83345 5.43706L1.83345 0.437057Z"
+    />
+  </svg>
+</template>
