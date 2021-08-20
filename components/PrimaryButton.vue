b77d27baba7762b73fe278107407db671dac83ca
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-20 18:09:15 +0200

    Add custom class

--- components/PrimaryButton.vue
+++ components/PrimaryButton.vue
@@ -1,6 +1,7 @@
 <template>
   <NuxtLink
     :to="destination"
+    :class="customClass"
     class="rounded-lg font-body bg-orange-500 shadow-md ring text-lg py-2 px-6 transition-all ring-orange-500 text-gray-50 gap-2 duration-300 items-center inline-flex hover:bg-orange-600 hover:ring-orange-600 hover:fill-orange-600 focus:outline-none"
   >
     <slot></slot>
@@ -16,6 +17,10 @@ export default {
       required: true,
     },
     destination: {
+      type: String,
+      default: '',
+    },
+    customClass: {
       type: String,
       default: '',
     },
