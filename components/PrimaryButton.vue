da6fba041d41843d73eaf9077071bd036f83b254
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-20 13:16:03 +0200

    Remove unused flex class

--- components/PrimaryButton.vue
+++ components/PrimaryButton.vue
@@ -1,7 +1,7 @@
 <template>
   <NuxtLink
     :to="destination"
-    class="rounded-lg flex font-body bg-orange-500 shadow-md ring text-lg py-2 px-6 transition-all ring-orange-500 text-gray-50 gap-2 duration-300 items-center inline-flex hover:bg-orange-600 hover:ring-orange-600 hover:fill-orange-600 focus:outline-none"
+    class="rounded-lg font-body bg-orange-500 shadow-md ring text-lg py-2 px-6 transition-all ring-orange-500 text-gray-50 gap-2 duration-300 items-center inline-flex hover:bg-orange-600 hover:ring-orange-600 hover:fill-orange-600 focus:outline-none"
   >
     <slot></slot>
     {{ label }}
