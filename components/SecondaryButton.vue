a8f25796ef21bdece3de87932205ee838dcdb7c2
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-21 22:25:48 +0200

    Remove unused class

--- components/SecondaryButton.vue
+++ components/SecondaryButton.vue
@@ -2,7 +2,7 @@
   <NuxtLink
     :to="destination"
     :class="customClass"
-    class="rounded-lg font-body shadow-md ring text-lg py-2 px-6 transition-all ring-green-500 text-green-500 gap-2 duration-300 items-center inline-flex hover:ring-green-600 hover:fill-green-600 hover:text-green-600 focus:outline-none"
+    class="rounded-lg font-body shadow-md ring text-lg py-2 px-6 transition-all ring-green-500 text-green-500 gap-2 duration-300 items-center inline-flex hover:ring-green-600 hover:text-green-600 focus:outline-none"
   >
     {{ label }}
     <slot></slot>
