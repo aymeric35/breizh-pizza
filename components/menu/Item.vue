cf11bee51c46e23b847e70d8d34630ce3f4b0581
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-21 22:24:59 +0200

    Replace svg divider with after css

--- components/menu/Item.vue
+++ components/menu/Item.vue
@@ -1,8 +1,7 @@
 <template>
   <div class="flex pb-4 lg:w-4/10">
     <div class="w-8/10">
-      <h3 class="title-h3 pb-0.5">{{ title }}</h3>
-      <MenuDividerLine></MenuDividerLine>
+      <h3 class="title-h3 pb-0.5 after:w-8 after:h-0.5 after:bg-orange-500 after:block after:mt-1 md:after:mt-2">{{ title }}</h3>
       <p class="font-body italic text-sm text-gray-200 pt-1 pr-6">{{ ingredients }}</p>
     </div>
     <div class="w-2/10">
@@ -29,3 +28,9 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+.title-h3::after {
+    content: "";
+}
+</style>
