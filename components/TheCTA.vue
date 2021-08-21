ee837e4c0624fa3080314d48ac0c3d789fb248a6
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-21 22:26:03 +0200

    Update CTA height on desktop

--- components/TheCTA.vue
+++ components/TheCTA.vue
@@ -2,7 +2,7 @@
   <section class="flex flex-col lg:flex-row">
     <div class="bg-gray-900 text-center text-gray-50 overflow-hidden w-full">
       <div
-        class="group bg-cta-order-m bg-cover bg-blend-overlay flex flex-col h-[50vh] lg:transition-all lg:duration-500 sm:bg-cta-order-hd ret:bg-cta-order-x2 lg:hover:transform lg:hover:scale-150 lg:hover:filter lg:hover:contrast-150 flex justify-center items-center lg:h-screen"
+        class="group bg-cta-order-m bg-cover bg-blend-overlay flex flex-col h-[50vh] lg:transition-all lg:duration-500 sm:bg-cta-order-hd ret:bg-cta-order-x2 lg:hover:transform lg:hover:scale-150 lg:hover:filter lg:hover:contrast-150 flex justify-center items-center lg:bg-center lg:h-[70vh]"
       >
         <div
           class="blur-scale-fix container space-y-4 lg:group-hover:transform lg:group-hover:scale-66 transition-transform duration-500"
@@ -17,7 +17,7 @@
     </div>
     <div class="bg-gray-900 text-center text-gray-50 overflow-hidden w-full">
       <div
-        class="group bg-cta-book-m bg-cover bg-blend-overlay flex flex-col h-[50vh] lg:transition-all lg:duration-500 sm:bg-cta-book-hd ret:bg-cta-book-x2 lg:hover:transform lg:hover:scale-150 lg:hover:filter lg:hover:contrast-150 flex justify-center items-center lg:h-screen"
+        class="group bg-cta-book-m bg-cover bg-blend-overlay flex flex-col h-[50vh] lg:transition-all lg:duration-500 sm:bg-cta-book-hd ret:bg-cta-book-x2 lg:hover:transform lg:hover:scale-150 lg:hover:filter lg:hover:contrast-150 flex justify-center items-center lg:h-[70vh]"
       >
         <div
           class="blur-scale-fix container space-y-4 lg:group-hover:transform lg:group-hover:scale-66 transition-transform duration-500"
