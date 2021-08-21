76eca985be91395fbc8332b0d675added13eb503
Aymeric <aymeric.bertrand35@gmail.com>
2021-08-21 22:25:31 +0200

    Create Title.vue

---
+++ components/menu/Title.vue
@@ -0,0 +1,22 @@
+<template>
+  <h2 :id="customID" :class="customClass" class="text-center pt-8 lg:pt-15 pb-2 title-h2">{{ content }}</h2>
+</template>
+
+<script>
+export default {
+  props: {
+    customID: {
+      type: String,
+      default: '',
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    customClass: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
